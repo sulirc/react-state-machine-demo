11c52e2939d25c5736c898a98a7274ee8e02b606
suli <ygj2awww@gmail.com>
2020-04-18 17:33:30 +0800

    add: add login machine

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,83 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal, Button, Input, Divider, Row, Col } from "antd";
-// import { useRef, useEffect, useCallback } from "react";
-import { Machine } from "xstate";
 import { useMachine } from "@xstate/react";
-
-const fetchUser = (userId, pwd) => {
-  console.info("post...", userId, pwd);
-  return new Promise((resolve, reject) => {
-    resolve({
-      msg: "ok",
-      code: 0,
-      data: {
-        level: Math.ceil(Math.random() * 30),
-        token: Math.random().toString(32).substring(2),
-      },
-    });
-  });
-};
-
-const loginMachine = Machine({
-  id: "login",
-  initial: "loggedOut",
-  states: {
-    loggedOut: {
-      on: {
-        SUBMIT: "loading",
-      },
-    },
-    loading: {
-      invoke: {
-        src: "postUserAuthData",
-        onDone: "loggedIn",
-        onError: "loggedOut",
-      },
-    },
-    loggedIn: {
-      on: {
-        LOGOUT: "loggedOut",
-      },
-    },
-  },
-});
+import loginMachine from "./loginMachine";
+import "./style.css";
 
 export default function LoginModal() {
-  const [state, send] = useMachine(loginMachine, {
-    services: {
-      postUserAuthData: (context, event) => {
-        return new Promise((resolve, reject) => {
-          resolve(true);
-        });
-      },
-    },
+  const [userMsg, setUserMsg] = useState({
+    account: "",
+    password: "",
   });
+  const [state, send] = useMachine(loginMachine);
+
+  function updateUserMsg(type, value) {
+    setUserMsg({
+      ...userMsg,
+      [type]: value,
+    });
+  }
+  function submit() {
+    send("SUBMIT", userMsg);
+  }
 
   return (
     <>
-      <h1>state: {state.value}</h1>
-      {
-        state.value === 'loggedIn' && <Button onClick={() => send('LOGOUT')}>Logout</Button>
-      }
+      <h1 className="state">Machine state: {state.value}</h1>
+      {state.context.tipMsg && <p className="tip-msg">tips: {state.context.tipMsg}</p>}
+      {state.value === "loggedIn" && <Button onClick={() => send("LOGOUT")}>Logout</Button>}
       <Modal
         title="Login"
+        closable={false}
+        mask={false}
         width={400}
         visible={state.value === "loggedOut"}
-        footer={<Button onClick={() => send("SUBMIT")}>Submit</Button>}
+        footer={<Button onClick={submit}>Submit</Button>}
       >
         <Row>
-          <Col span={6}>ID:</Col>
+          <Col span={6}>Account:</Col>
           <Col span={18}>
-            <Input placeholder="please enter ur userId" />
+            <Input placeholder="please enter account" value={userMsg.account} onChange={(e) => updateUserMsg("account", e.target.value)} />
           </Col>
         </Row>
-        <Divider orientation="left" style={{ color: "#333", fontWeight: "normal", fontSize: '12px' }}>
+        <Divider orientation="left" style={{ color: "#333", fontWeight: "normal", fontSize: "12px" }}>
           Fill Password
         </Divider>
         <Row>
           <Col span={6}>Password:</Col>
           <Col span={18}>
-            <Input.Password placeholder="please enter ur password" />
+            <Input.Password
+              placeholder="please enter password"
+              value={userMsg.pwd}
+              onChange={(e) => updateUserMsg("password", e.target.value)}
+            />
           </Col>
         </Row>
       </Modal>
---
+++ src/components/loginMachine.js
@@ -0,0 +1,129 @@
+import { createMachine as Machine, assign } from "xstate";
+
+const postUserAuthData = ({ account, password }) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (account === 'suli' && password === 'qwer1234') {
+        resolve({
+          msg: "login ok",
+          code: 0,
+          data: {
+            token: `${account}/${password}.${Math.random().toString(32).substring(2)}`,
+          },
+        });
+      } else {
+        reject({
+          msg: "login fail",
+          code: -1,
+        });
+      }
+    }, 1000);
+  });
+};
+
+const setUserToken = assign({
+  token: (_ctx, evt) => {
+    return evt.data.data.token;
+  },
+});
+
+const clearUserToken = assign({
+  token: (_ctx, evt) => {
+    return null;
+  },
+});
+
+const updateTipMsg = assign({
+  tipMsg: (ctx, evt) => {
+    if (formIsInvalid(ctx)) {
+      return "form invalid";
+    }
+    if (evt.type === "LOGOUT") {
+      return "logout ok";
+    }
+    return evt.data.msg;
+  },
+});
+
+const updateUserFormData = assign({
+  account: (_ctx, evt) => {
+    return evt.account;
+  },
+  password: (_ctx, evt) => {
+    return evt.password;
+  },
+});
+
+function formIsInvalid(ctx, evt) {
+  return !(ctx.account && ctx.password);
+}
+
+export default Machine(
+  {
+    id: "login",
+    initial: "loggedOut",
+    context: {
+      account: null,
+      password: null,
+      token: null,
+      tipMsg: "",
+    },
+    states: {
+      loggedOut: {
+        on: {
+          SUBMIT: {
+            target: "loading",
+            actions: "updateUserFormData",
+          },
+        },
+      },
+      loading: {
+        on: {
+          "": {
+            target: "loggedOut",
+            actions: "updateTipMsg",
+            cond: "formIsInvalid",
+          },
+        },
+        invoke: {
+          src: "login",
+          onDone: {
+            target: "loggedIn",
+            actions: ["setUserToken", "updateTipMsg"],
+          },
+          onError: {
+            target: "loggedOut",
+            actions: ["clearUserToken", "updateTipMsg"],
+          },
+        },
+      },
+      loggedIn: {
+        on: {
+          LOGOUT: {
+            target: "loggedOut",
+            actions: ["clearUserToken", "updateTipMsg"],
+          },
+        },
+      },
+    },
+  },
+  {
+    services: {
+      login: (ctx, evt) => {
+        return postUserAuthData({
+          account: ctx.account,
+          password: ctx.password,
+        });
+      },
+    },
+    actions: {
+      setUserToken,
+      clearUserToken,
+      updateTipMsg,
+      updateUserFormData,
+    },
+    guards: {
+      formIsInvalid,
+    },
+  }
+);
