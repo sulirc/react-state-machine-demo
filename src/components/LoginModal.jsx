9069d45e41eda1177e4a15ab96be644b42904de8
suli <ygj2awww@gmail.com>
2020-04-18 17:48:22 +0800

    change useState to useRef

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,31 +1,39 @@
-import React, { useState } from "react";
-import { Modal, Button, Input, Divider, Row, Col } from "antd";
+import React, { useRef } from "react";
+import { curry } from "lodash";
 import { useMachine } from "@xstate/react";
+import { Modal, Button, Input, Divider, Row, Col } from "antd";
 import loginMachine from "./loginMachine";
 import "./style.css";
 
 export default function LoginModal() {
-  const [userMsg, setUserMsg] = useState({
+  const [state, send] = useMachine(loginMachine);
+  const userMsg = useRef({
     account: "",
     password: "",
   });
-  const [state, send] = useMachine(loginMachine);
 
-  function updateUserMsg(type, value) {
-    setUserMsg({
-      ...userMsg,
-      [type]: value,
+  function submit() {
+    send("SUBMIT", userMsg.current);
+  }
+  function loggout() {
+    send("LOGOUT");
+  }
+
+  function updateUserMsg(type, e) {
+    userMsg.current = ({
+      ...userMsg.current,
+      [type]: e.target.value,
     });
   }
-  function submit() {
-    send("SUBMIT", userMsg);
-  }
+
+  const updateAccount = curry(updateUserMsg)("account");
+  const updatePassword = curry(updateUserMsg)("password");
 
   return (
     <>
       <h1 className="state">Machine state: {state.value}</h1>
       {state.context.tipMsg && <p className="tip-msg">tips: {state.context.tipMsg}</p>}
-      {state.value === "loggedIn" && <Button onClick={() => send("LOGOUT")}>Logout</Button>}
+      {state.value === "loggedIn" && <Button onClick={loggout}>Logout</Button>}
       <Modal
         title="Login"
         closable={false}
@@ -37,7 +45,7 @@ export default function LoginModal() {
         <Row>
           <Col span={6}>Account:</Col>
           <Col span={18}>
-            <Input placeholder="please enter account" value={userMsg.account} onChange={(e) => updateUserMsg("account", e.target.value)} />
+            <Input placeholder="please enter account" defaultValue={userMsg.account} onChange={updateAccount} />
           </Col>
         </Row>
         <Divider orientation="left" style={{ color: "#333", fontWeight: "normal", fontSize: "12px" }}>
@@ -46,11 +54,7 @@ export default function LoginModal() {
         <Row>
           <Col span={6}>Password:</Col>
           <Col span={18}>
-            <Input.Password
-              placeholder="please enter password"
-              value={userMsg.pwd}
-              onChange={(e) => updateUserMsg("password", e.target.value)}
-            />
+            <Input.Password placeholder="please enter password" defaultValue={userMsg.pwd} onChange={updatePassword} />
           </Col>
         </Row>
       </Modal>
